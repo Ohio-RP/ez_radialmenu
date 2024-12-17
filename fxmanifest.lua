4ab7c6bf3ebbd53d7312fd571954f627d9ddf76c
Rayaan Uddin <rayaan.uddin@outlook.com>
2024-12-17 22:54:43 +0000

    Updated version control

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,9 @@ game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 name 'RadialMenu'
-description 'RadialMenu for RedM'
+description '[VORP] A radial menu for RedM'
 author 'Rayaan Uddin'
-version '1.0.0'
+version '1.0'
 
 lua54 'yes'
 
